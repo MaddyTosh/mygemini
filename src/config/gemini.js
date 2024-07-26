7bcd8eda55b69fe8f02de2c13d9a16c931df102f
MaddyTosh <toshniwalmadhavan@gmail.com>
2024-07-27 03:57:05 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,49 @@
+
+//AIzaSyDsgmtqG5EyCd7Jr43e54SVaS1XC6MDPgI
+
+
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyAwQ3H7XBBH7rfsLx6qKZxGQJ44ZW8IuEw";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.0-pro",
+  });
+  
+  const generationConfig = {
+    temperature: 0.9,
+    topP: 1,
+    maxOutputTokens: 2048,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+ export default run;
